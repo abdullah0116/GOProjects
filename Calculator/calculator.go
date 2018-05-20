c9053ea3802f7153c2d3376bb92c959f865054bf
abdullahkareem <akasak56@gmail.com>
2018-05-20 11:34:12 -0600

    cleaning up code

--- Calculator/calculator.go
+++ Calculator/calculator.go
@@ -7,27 +7,27 @@ import (
 
 func main() {
 	var firstNumber float64
-	var opperation string  
+	var opperation string
 	var secondNumber float64
 
-	fmt.Print("please enter the first number you'd like to add: ")
+	fmt.Print("please enter the first number you'd like to calculate: ")
 	fmt.Scan(&firstNumber)
-	
-	fmt.Print("please enter the first number you'd like to add: ")
+
+	fmt.Print("Please enter the opperation you would like to use: ")
 	fmt.Scan(&opperation)
 
-	fmt.Print("please enter the first number you'd like to add: ")
+	fmt.Print("please enter the second number you'd like to calculate: ")
 	fmt.Scan(&secondNumber)
 
-	if (opperation == "+") {
-		fmt.Println("Here's your answer: ", firstNumber + secondNumber)
-	} else if (opperation == "-") {
-		fmt.Println("Here's your answer: ", firstNumber - secondNumber)
-	} else if (opperation == "*") {
-		fmt.Println("Here's your answer: ", firstNumber * secondNumber)
-	} else if (opperation == "/") {
-		fmt.Println("Here's your answer: ", firstNumber / secondNumber)
-	} else if (opperation == "%") {
+	if opperation == "+" {
+		fmt.Println("Here's your answer: ", firstNumber+secondNumber)
+	} else if opperation == "-" {
+		fmt.Println("Here's your answer: ", firstNumber-secondNumber)
+	} else if opperation == "*" || opperation == "x" {
+		fmt.Println("Here's your answer: ", firstNumber*secondNumber)
+	} else if opperation == "/" {
+		fmt.Println("Here's your answer: ", firstNumber/secondNumber)
+	} else if opperation == "%" {
 		fmt.Println("Here's your answer: ", math.Mod(firstNumber, secondNumber))
 	} else {
 		fmt.Println("Sorry cant find this sign: ", opperation)
